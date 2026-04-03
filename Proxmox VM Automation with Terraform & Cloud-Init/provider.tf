61604801c1427cd9fb0690de00c10f464136e7e7
Sakora <geoffreysakora@gmail.com>
2026-04-03 20:22:08 +0300

    renaming

--- Proxmox VM Automation with Terraform & Cloud-Init/provider.tf
+++ Proxmox VM Automation with Terraform & Cloud-Init/provider.tf
@@ -8,11 +8,7 @@ terraform {
 }
 
 provider "proxmox" {
-<<<<<<< HEAD:Proxmox VM Automation with Terraform & Cloud-Init/provider.tf
     endpoint  = "https://IP/" #proxmox GUI page
-=======
-    endpoint  = "https://IP/"
->>>>>>> b3e3907c23e7aebc7e757dfeac267d458e0109ba:Proxmox/provider.tf
     api_token = "user@pve!token-name=your-token"
     insecure  = "true" #because of the self-signed SSL certs
 }
