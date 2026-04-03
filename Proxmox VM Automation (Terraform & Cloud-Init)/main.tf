597411872eb43e1d93c410567f8ccf34d401587d
Sakora <geoffreysakora@gmail.com>
2026-04-03 20:32:03 +0300

    tf2

--- Proxmox VM Automation (Terraform & Cloud-Init)/main.tf
+++ Proxmox VM Automation (Terraform & Cloud-Init)/main.tf
@@ -1,6 +1,6 @@
 resource "proxmox_virtual_environment_vm" "iso_vm" {
     name      = "terraform-test-01"
-    node_name = "t41"        #replace with your node name 
+    node_name = "t41"               #replace with your node name 
     vm_id     = 104                 #id number in your list
 
     cpu {
@@ -25,7 +25,7 @@ resource "proxmox_virtual_environment_vm" "iso_vm" {
       #enabled = true  #is by true by default since we are using the cdrom{} block
       file_id = "local:iso/ubuntu-24.04.3-live-server-amd64.iso"
     }
-    
+
     initialization {
       datastore_id = "local-lvm"
     }
